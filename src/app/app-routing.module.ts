9dfc0cb59f40e82f2f6bbdde3da9907a41e7d58b
huxley <elhuxley277@gmail.com>
2023-08-16 00:17:59 -0500

    feat(register): registercomponent.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './pages/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Otras rutas...
+  { path: 'register', component: RegisterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+
+
+
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
+
 export class RegisterComponent {
+  tittle = "Register";
+  public allitem: any = []
+
+  addItem(your_name: string, email:string, password: string){
+    const newTask = {
+      user_name: your_name,
+      user_email: email,
+      user_password:password
+
+    }
+  }
+
 
 }
