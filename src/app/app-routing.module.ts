a7652f2baa8041d1e2d276e170541c169cdce2f9
nicolas <jaljordan77@gmail.com>
2023-08-16 18:25:38 -0500

    update->src/app/app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-import { RegisterComponent } from './pages/register/register.component';
-
-const routes: Routes = [
-  // Otras rutas...
-  { path: 'register', component: RegisterComponent },
-
 import { LoginComponent } from './pages/login/login.component';
-
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
 const routes: Routes = [
-  {path:'',component:LoginComponent}
+
+  { path: 'register', component: RegisterComponent },
+  {path:'login',component:LoginComponent},
+  {path:'home',component:HomeComponent}
 
 ];
 
