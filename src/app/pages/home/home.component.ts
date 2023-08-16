99c31fe6e13fb77f0296ebecd8127b44a5c462c0
John9135 <palacioalexander5@gmail.com>
2023-08-16 10:55:29 -0500

    update: The 'output' function was implemented in 'header.component' to pass the data to 'home.component', and it was just completed by integrating the 'reactions' and 'comments' components

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,37 +1,26 @@
 import { outputAst } from '@angular/compiler';
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { Home } from 'src/app/models/item'
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  template: '<app-home (addModel) = "receiveData($event)>"</app-home>',
 })
 export class HomeComponent {
   title = 'home';
 
-  // allItems = [{
-  //   user: "Javier",
-  //   description: "Estudio de boodcamp con academy",
-  //   image: "https://process.filestackapi.com/resize=width:600,height:315,fit:max/quality=value:90/XCJiXIchRDmj0ORyMCRv",
-  //   reactions: "false",
-  //   comments: "Es un gran programa"
-  // }
-  //]
-
-  @Output() addModel: EventEmitter<{
-    user: String, description: String,
-    image: String, comments: String
-  }> = new EventEmitter<{
-    user: String, description: String,
-    image: String, comments: String
-  }>
-
-  addHomeModel(user: String, description: String,
-    image: String, comments: String) {
-    this.addModel.emit({
-      user, description,
-      image, comments
-    })
+  items = {
+    reactions: 'false',
+    comments: 'Es un excelente bootcamp',
+  }
+  receiveData(data: {
+    user: String,
+    description: String,
+    image: String
+  }) {
+    console.log('Data received:', data);
   }
 }
