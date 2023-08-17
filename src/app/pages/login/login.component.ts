aee1b3312d3d22ee2866b1eca6232f9d2ace34b2
maribelaristizabal <maribelaristizabal079@gmail.com>
2023-08-17 17:37:51 -0500

    [login.component.ts]

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent {
 
+  tittle = "Register";
+  public allitem: any = []
+
+  addItem(email:string, password: string){
+    const newTask = {
+     
+      email: email,
+      password:password
+
+    }
+  }
+
+  contactForm!:FormGroup;
+
+  constructor(private readonly fb: FormBuilder) {}
+  
+  ngOnInit(): void {
+    this.contactForm = this.initFrom();
+  }
+  onSubmit(): void { 
+    console.log('form ->',this.contactForm.value);
+  }
+
+  initFrom(): FormGroup {
+    return this.fb.group ({
+      email: ['',[Validators.required]],
+      password: ['',[Validators.required,Validators.minLength(8)]],
+    })
+ }
 }
+
 //simulacion bakcend
