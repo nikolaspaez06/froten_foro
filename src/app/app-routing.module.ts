7373bf4b5e4bbad666be64fabb3f51df8222763f
maribelaristizabal <maribelaristizabal079@gmail.com>
2023-08-17 11:32:29 -0500

    update[app-routing.module.ts]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+
 
 const routes: Routes = [
   // Otras rutas...
   { path: 'register', component: RegisterComponent },
+  { path:'login',component:LoginComponent},
 
-import { LoginComponent } from './pages/login/login.component';
-
-const routes: Routes = [
-  {path:'',component:LoginComponent}
 
 ];
 
