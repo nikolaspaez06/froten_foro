9a07d1e8d44c3216622b96204f2b3b58bc03c62b
Juan Andrés Ruiz <ruizvalencia78@gamil.com>
2023-08-17 14:42:21 -0500

    odified[angular.json]

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 const routes: Routes = [
-  {path:'',component:LoginComponent}
+  {path:'',component:LoginComponent},
+  {path:'profile',component:ProfileComponent}
 ];
 
 @NgModule({
