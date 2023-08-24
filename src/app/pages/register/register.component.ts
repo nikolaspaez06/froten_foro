6206b071a5d59cce11fb34be501fc12a6ce81386
nicolas <jaljordan77@gmail.com>
2023-08-24 11:45:51 -0500

    update->src/app/pages/register/register.component.ts

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
-
 import { FormBuilder,FormGroup,Validators } from '@angular/forms';
 import { AuthService } from 'src/app/core/service/auth.service';
 import { Router } from '@angular/router';
 
-
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ForoService } from 'src/app/core/service/foro.service';
 
 
 @Component({
@@ -22,8 +18,7 @@ export class RegisterComponent {
     private readonly fb: FormBuilder,
     private router:Router){}
 
-  contactForm!: FormGroup;
-
+  contactForm!:FormGroup;
 
   ngOnInit(): void {
     this.contactForm = this.initFrom();
@@ -39,57 +34,18 @@ export class RegisterComponent {
     },
     err => console.log(err)
     )
-=======
-  constructor(private foroService: ForoService, private readonly fb: FormBuilder) { }
-
-  ngOnInit(): void {
-    this.contactForm = this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(3)]],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(8)]]
-    });
-
   }
 
 
   onSubmit(): void {
-    if (this.contactForm.valid) {
-      const newUser = {
-        name: this.contactForm.value.name,
-        email: this.contactForm.value.email,
-        password: this.contactForm.value.password,
-        admin: ['user']
-      };
-
-      this.foroService.addUser(newUser).subscribe(
-        response => {
-          console.log('Formulario enviado exitosamente:', response);
-          alert('Usuario añadido exitosamente');
-        },
-        error => {
-          console.error('Error al enviar el formulario:', error);
-        }
-      );
-    } else {
-      // Error message.
-    }
+    console.log('form ->',this.contactForm.value);
   }
 
-
   initFrom(): FormGroup {
-
     return this.fb.group ({
       userName: ['',[Validators.required,Validators.minLength(3)]],
       email: ['',[Validators.required]],
       password: ['',[Validators.required,Validators.minLength(8)]],
-
-    return this.fb.group({
-      name: ['', [Validators.required, Validators.minLength(3)]],
-      email: ['', [Validators.required]],
-      password: ['', [Validators.required, Validators.minLength(8)]],
-
-
     })
-  }
 }
-
+}
