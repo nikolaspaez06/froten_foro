a16808761725c406a5921e976036e5bde782f3c7
sewastian <sebastianrincon834@gmail.com>
2023-08-24 10:00:36 -0500

    fear [guards -> auth.guard.ts]

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,19 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from 'src/app/core/service/auth.service';
+
+
+
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService)
+  const router = inject(Router)
+
+  if(authService.loggedIn()){
+    return true
+  }else{
+    alert('access denied')
+    router.navigate([''])
+    return false
+  }
+};
