8ad33fb9f00e502c71f1be6231c5b11e17a42480
sewastian <sebastianrincon834@gmail.com>
2023-08-25 12:19:43 -0500

    created [token -> token.services]

---
+++ src/app/core/service/token/token.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService implements HttpInterceptor {
+
+  constructor(private authService: AuthService) { }
+
+  intercept(req:any, next:any) {
+    const tokennzeReq = req.clone({
+      setHeaders: {
+        Authorizatiion: `Bearer ${this.authService.gettoken()}`
+      }
+    })
+    return next.handle(tokennzeReq)
+
+  }
+}
