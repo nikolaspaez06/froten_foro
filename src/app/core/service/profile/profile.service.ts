6b666a18c7aed00dda25b34960089b7b34ad0a53
Brayan <brayantandap@gmail.com>
2023-08-26 16:06:58 -0500

    feat(profile): Retrieve and display user data by _id
    - Trying fetching user data based on their ID and login token.
    The goal is to display the necessary user information in the HTML template.

--- src/app/core/service/profile/profile.service.ts
+++ src/app/core/service/profile/profile.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/item';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  private URL = 'http://localhost:3000/poofo'
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.URL);
+  }
+
+  getUser(_id: string): Observable<User[]> {
+    return this.http.get<User[]>(`${this.URL}/${_id}`);
+  }
 }
--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from 'src/app/core/service/profile/profile.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/models/item';
+import { AuthService } from 'src/app/core/service/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,11 +10,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent {
-  title = 'profile';
+  users: User[] = [];
+  user: User[] = [];
 
-  allItems = {
-    user: 'Alexander',
-    description: 'Trabajo de foro',
-    image: 'https://process.filestackapi.com/resize=width:600,height:315,fit:max/quality=value:90/XCJiXIchRDmj0ORyMCRv',
-  };
+  constructor(private router: Router, private activerouter: ActivatedRoute, private ProfileService: ProfileService) { }
+
+  ngOnInit(): void {
+    const _id = this.activerouter.snapshot.paramMap.get('id');
+    const token = this.gettoken();
+    console.log(token);
+
+    if (_id) {
+      this.ProfileService.getUser(_id).subscribe(data => {
+        this.user = data;
+        console.log(data);
+      });
+    }
+  }
+
+  gettoken() {
+    return localStorage.getItem('token')
+  }
+
+  getUsers() {
+    this.ProfileService.getUsers().subscribe(data => {
+      this.users = data;
+    })
+  }
+  // getUser(_id: string) {
+  //   this.ProfileService.getUser(_id).subscribe(data => {
+  //     this.user = data;
+  //   })
+  // }
+  editUser(_id: string) {
+    console.log(_id)
+  }
+
+  // title = 'profile';
+
+  // allItems = {
+  //   user: 'Alexander',
+  //   description: 'Trabajo de foro',
+  //   image: 'https://process.filestackapi.com/resize=width:600,height:315,fit:max/quality=value:90/XCJiXIchRDmj0ORyMCRv',
+  // };
 }
