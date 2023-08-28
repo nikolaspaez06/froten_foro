5c5e8a555284f00a9b852385cbab76f735fffbd3
sewastian <sebastianrincon834@gmail.com>
2023-08-28 08:55:45 -0500

    update [services/interaction -> interaction.service.ts]

--- src/app/core/service/interaction/interaction.service.ts
+++ src/app/core/service/interaction/interaction.service.ts
@@ -10,10 +10,12 @@ export class InteractionService {
   private apiUrl = 'https://pooforoapi.onrender.com/'
   constructor(private http: HttpClient) { }
 
+  // Url Peticion Estado de Like
   likePublication(publicationId: string):Observable<any>{
     return this.http.patch(`${this.apiUrl}interactions/update/${publicationId}`,{like: true})
   }
 
+  // Url Peticion Remover Like
   unlikePublication(publicationId: string): Observable<any> {
     return this.http.delete(`${this.apiUrl}interactions/delete/${publicationId}`);
   }
